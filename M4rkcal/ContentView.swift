111e249323f2b194bf9e567d475b44cb4ec30c13
Marcos <rodriguezmarcos.fi@gmail.com>
2026-08-26 08:48:06 +0300

    Initial Commit

---
+++ M4rkcal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  M4rkcal
+//
+//  Created by Marcos Rodríguez Hernández on 26.8.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ M4rkcal/M4rkcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4rkcalApp.swift
+//  M4rkcal
+//
+//  Created by Marcos Rodríguez Hernández on 26.8.2026.
+//
+
+import SwiftUI
+
+@main
+struct M4rkcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
